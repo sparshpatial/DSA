20c984f406ba86dfe8bc53da6a677e886054ae9d
Sparsh Patial <patial.sparsh5@gmail.com>
2023-01-15 00:30:11 +0530

    testing file

---
+++ src/Java/Main.java
@@ -0,0 +1,8 @@
+package Java;
+
+public class Main {
+    public static void main(String[]args){
+        System.out.println("Basic test java program all do once,");
+        System.out.println("Hello world!");
+    }
+}
