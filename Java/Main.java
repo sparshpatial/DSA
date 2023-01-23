e02fe7f8b48b00b9f1ec2369dc9e02059a432880
Sparsh Patial <patial.sparsh5@gmail.com>
2023-01-23 20:33:07 +0530

    input scanner

--- Java/Main.java
+++ Java/Main.java
@@ -1,8 +1,16 @@
-package Java;
 
+import java.util.Scanner;
 public class Main {
     public static void main(String[]args){
         System.out.println("Basic test java program all do once,");
         System.out.println("Hello world!");
+
+        Scanner input =new Scanner(System.in);
+        System.out.print("Enter an Integer: ");
+        System.out.println(input.nextInt());
+       
+
+
+
     }
 }
